88982a851b997659b380111daa7cf5d67567bd2a
yuchuan <yuchuan@yuchuan.com>
2019-10-06 21:27:03 -0400

    Auth Demo APP -L105

---
+++ server/router.js
@@ -0,0 +1,9 @@
+const Authentication = require('./controllers/authentication');
+
+module.exports = function (app) {
+    // app.get('/', function (req, res, next) {
+    //     res.send(['waterbottle', 'phone', 'paper']);
+    // });
+    app.post('/signup', Authentication.signup);
+
+}
